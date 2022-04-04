15d0303ab6a0b984e2affbb08fb2a9fa46b08fe2
benedly <benedly.kim@gmail.com>
2022-04-04 13:27:30 +0900

    babel

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // add lodash
 import * as _ from 'lodash';
 
-// style
-import "./assets/scss/style.scss";
-
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = getMsg()
 
     return element;
 }
+
+const getMsg = () => 'Hello Babel'
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
 const path = require("path");
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.ts",
+  entry: ["./src/index.ts", "./src/assets/scss/style.scss"],
   output: {
-    filename: "bundle.js", // webpack default main.js
     path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js", // webpack default main.js
   },
-  plugins: [
-    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
-    new MiniCssExtractPlugin({ filename: 'bundle.css' })
-  ],
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    useBuiltIns: "usage",
+                    corejs: "3.6.4",
+                    targets: {
+                      chrome: "87",
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
       {
         test: /\.s[ac]ss$/i,
         use: [
@@ -25,14 +45,20 @@ module.exports = {
           // Compiles Sass to CSS
           "sass-loader",
         ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       // TypeScript 로더 설정
       {
         test: /\.tsx?$/i,
-        use: ['ts-loader']
-      }
+        use: ["ts-loader"],
+      },
     ],
   },
-  mode: "production",
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: "bundle.css" }),
+    new HtmlWebpackPlugin()
+  ],
+  devtool: "source-map",
+  mode: "development",
 };
